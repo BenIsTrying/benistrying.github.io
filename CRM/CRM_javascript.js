76b071fbd68b70a0420870e20aa95b492bbd9290
BenIsTrying <benjamin.george-3@students.plymouth.ac.uk>
2024-09-14 20:37:47 +0100

    added navbar icons

--- CRM/CRM_javascript.js
+++ CRM/CRM_javascript.js
@@ -7,7 +7,7 @@ $(function() {
 
         if ((username == "test") && (password == "password")){
             console.log("hooray you've logged in");
-            window.location = 'Main.html';//allows user to access the other pages
+            window.location = 'CRM_admin.html';//allows user to access the other pages
         }
         else {
             alert("Incorrect, please enter you details again");
